85042a3478a436a75de77a1ce8b60721e96c7563
Stylix <78728283+AiEmStylix@users.noreply.github.com>
2024-01-21 15:38:58 +0700

    First Commit

---
+++ Window.h
@@ -0,0 +1,7 @@
+#ifndef WINDOW_H 
+#define WINDOW_H
+
+#include <SDL2/SDL.h>
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
+using namespace std;
 const int WIDTH = 800, HEIGHT = 600;
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window *window = NULL;
 
-    if (NULL == window)
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-        return 1;
+        cout << "Could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
     }
+    else
+    {
+        window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+        if (window == NULL)
+        {
+            std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+            return 1;
+        }
+    }
+
     SDL_Event windowEvent;
     while (true)
     {
